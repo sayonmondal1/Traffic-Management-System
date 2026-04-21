1fe6d66c979f02db1ea54671313e791f3fd83f20
Sayon Mondal <sayonmondal074@gmail.com>
2026-04-21 20:17:56 +0530

    Updated training logic

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from ultralytics import YOLO
 import cv2
 from sort.sort import *
 from util import get_car, read_license_plate, write_csv
+import numpy as np
 results = {}
 mot_tracker = Sort()
 # Load model
 coco_model = YOLO('models/yolov8x.pt')
-license_plate_detector = YOLO('./runs/detect/train/weights/best.pt')
+license_plate_detector = YOLO('./runs/detect/train2/weights/best.pt')
 # Load video
 cap = cv2.VideoCapture('./videos/vehicles2.mp4')
 vehicles = [2, 3, 5, 7]
@@ -17,8 +18,8 @@ while ret:
     frame_nmr +=1
     ret, frame = cap.read()
     if ret:
-        if frame_nmr > 10:
-            break
+        # if frame_nmr > 10:
+        #     break
         results[frame_nmr] = {}
         # Detect vhehicles 
         detections = coco_model(frame)[0]
@@ -36,21 +37,38 @@ while ret:
             x1, y1, x2, y2, score, class_id = license_plate
             # Assign license plate to car
             xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+            if car_id == -1:
+                continue
             # Crop license plate
-            license_plate_crop = frame[int(y1):int(y2), int(x1), int(x2), :]
+            # license_plate_crop = frame[int(y1):int(y2), int(x1), int(x2), :]
+            license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2)]
             # Process license palte
             license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
             _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
             cv2.imshow('original_crop', license_plate_crop)
             cv2.imshow('threshold', license_plate_crop_thresh)
-            cv2.waitKey(0)
+            #cv2.waitKey(0)
+            cv2.waitKey(1)
             # Read license plate number.
             license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
-            if license_plate_text is not None:
-                results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
-                                                  'license_plate': {'bbox': [x1, y1, x2, y2],
-                                                                    'text': license_plate_text,
-                                                                    'bbox_score': score,
-                                                                    'text_score': license_plate_text_score}}
+            print("OCR:", license_plate_text)
+            # if license_plate_text is not None:
+            #     results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+            #                                       'license_plate': {'bbox': [x1, y1, x2, y2],
+            #                                                         'text': license_plate_text,
+            #                                                         'bbox_score': score,
+            #                                                         'text_score': license_plate_text_score}}
+            if license_plate_text is not None and car_id != -1:
+                print("Detected:", license_plate_text)
+
+                results[frame_nmr][car_id] = {
+                    'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
+                    'license_plate': {
+                    'bbox': [x1, y1, x2, y2],
+                    'text': license_plate_text,
+                    'bbox_score': score,
+                    'text_score': license_plate_text_score
+        }
+    }
 # Write results
 write_csv(results, './CSV_Files/test.csv')
--- train.py
+++ train.py
@@ -1,10 +1,18 @@
 from ultralytics import YOLO
-# Load model and dataset
-model = YOLO("models/yolov8n.pt")
-model.train(data="D:/Programming/Python/Python Projects/Traffic_Management_System/data.yaml", epochs=200, device="cpu")
-"""
 import torch
-print("Using device:", torch.cuda.get_device_name(0))
-model = YOLO("yolov8n.pt")
-model.train(data=r"D:/Programming/Python/Python Projects/Traffic_Management_System/data.yaml", epochs=200, device=0)  # instead of "cuda"
-"""
+# model = YOLO("models/yolov8n.pt")
+# model.train(data="D:/Programming/Python/Python Projects/Traffic_Management_System/data.yaml", epochs=200, device=0)
+def main():
+    print("CUDA available:", torch.cuda.is_available())
+    print("Using:", torch.cuda.get_device_name(0))
+    model = YOLO("models/yolov8n.pt")
+    model.train(
+        data="D:/Programming/Python/Python Projects/Traffic_Management_System/data.yaml",
+        epochs=200,
+        imgsz=640,
+        batch=8,
+        device=0,     
+        workers=0  
+    )
+if __name__ == "__main__":
+    main()
