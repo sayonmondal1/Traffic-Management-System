709455ebb73002a4d2d56410e22929d30b4cf11a
Sayon Mondal <sayonmondal074@gmail.com>
2026-05-13 20:20:42 +0530

    Removed vehicle_counter_check.py

--- check.py
+++
@@ -1,5 +0,0 @@
-import torch
-print("CUDA available:", torch.cuda.is_available())
-print("Torch version:", torch.__version__)
-if torch.cuda.is_available():
-    print("Device name:", torch.cuda.get_device_name(0))
